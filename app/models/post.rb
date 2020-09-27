395833958690ecc202fa5615d71ddf62fa201a2b
Miguel Macias <miguelfernandomacias@gmail.com>
2020-09-27 18:51:16 -0400

    modelo y vistas

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+  validates :title, presence:true
+  validates :content, presence:true
+  validates :content, exclusion: {in: %w[maldito, aberrante]}
+  validates :image_url, presence:true
 end
